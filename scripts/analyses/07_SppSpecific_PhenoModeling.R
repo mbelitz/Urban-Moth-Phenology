0e75712e4466f217f6a0a849b1932a7781626e77
mbelitz <mbelitz@ufl.edu>
2023-08-30 15:57:10 -0400

    vif checking

--- scripts/analyses/07_SppSpecific_PhenoModeling.R
+++ scripts/analyses/07_SppSpecific_PhenoModeling.R
@@ -110,8 +110,15 @@ mdf <- mdf %>%
 
 mdf <- na.omit(mdf)
 
-
 # peak modeling
+# Dev1
+#checking for collinearity among predictors (so no interactions included)
+m_col <- lmer(formula = peak ~ Dev_1 + voltinism + maxWingspan + 
+            hostPlantSepcialization + bio1_mean + bio1_sd +
+            (1|validName),
+          data = mdf, REML = F)
+performance::check_collinearity(m_col)
+
 m <- lmer(formula = peak ~ Dev_1 + voltinism + maxWingspan + 
             hostPlantSepcialization + bio1_mean + bio1_sd +
             Dev_1:voltinism +
@@ -119,17 +126,18 @@ m <- lmer(formula = peak ~ Dev_1 + voltinism + maxWingspan +
             Dev_1:hostPlantSepcialization +
             Dev_1:bio1_mean +
             Dev_1:bio1_sd +
-            (1 | Site)+ (1|validName),
+            (1|validName),
           data = mdf, REML = F)
+summary(m)
+confint(m)
 
-ms <- step(m)
-ms
-m_top <- lmer(formula = peak ~ voltinism +
-                (1|validName),
-              data = mdf, REML = F)
-summary(m_top)
+# Dev10 model
 
-step(m_top)
+m2_col <- lmer(formula = peak ~ Dev_10 + voltinism +  maxWingspan + 
+             hostPlantSepcialization + bio1_mean + bio1_sd +
+             (1|validName),
+           data = mdf, REML = F)
+performance::check_collinearity(m2_col)
 
 m2 <- lmer(formula = peak ~ Dev_10 + voltinism +  maxWingspan + 
              hostPlantSepcialization + bio1_mean + bio1_sd +
@@ -138,13 +146,18 @@ m2 <- lmer(formula = peak ~ Dev_10 + voltinism +  maxWingspan +
              Dev_10:hostPlantSepcialization +
              Dev_10:bio1_mean +
              Dev_10:bio1_sd +
-             (1 | Site)+ (1|validName),
+             (1|validName),
            data = mdf, REML = F)
 
-m2s <- step(m2)
-m2s
-m2_top <- lmer(formula = peak ~ voltinism + (1|validName),
-               data = mdf, REML = F)
+summary(m2)
+confint(m2)
+
+#rel temp modeling
+m3_col <- lmer(formula = peak ~ rel_temp + voltinism  + maxWingspan + 
+             hostPlantSepcialization + bio1_mean + bio1_sd +
+             (1|validName),
+           data = mdf, REML = F)
+performance::check_collinearity(m3_col)
 
 m3 <- lmer(formula = peak ~ rel_temp + voltinism  + maxWingspan + 
              hostPlantSepcialization + bio1_mean + bio1_sd +
@@ -153,37 +166,28 @@ m3 <- lmer(formula = peak ~ rel_temp + voltinism  + maxWingspan +
              rel_temp:hostPlantSepcialization +
              rel_temp:bio1_mean +
              rel_temp:bio1_sd +
-             (1 | Site)+ (1|validName),
+             (1|validName),
            data = mdf, REML = F)
-m3s <- step(m3)
-m3s
 
-m3_top <-  lmer(formula = peak ~ rel_temp + voltinism + maxWingspan + bio1_mean +
-                  rel_temp:bio1_mean + rel_temp:maxWingspan +
-                  (1|validName),
-                data = mdf, REML = F)
-step(m3_top)
-summary(m3_top)
-car::vif(m3_top)
+summary(m3)
+confint(m3)
 
-AICc(m_top, m2_top, m3_top)# m3 lowest
-Weights(AICc(m_top, m2_top, m3_top))# m3 clear top model # m_top & m_2 are same model so reporting one
-AICc( m2_top, m3_top)# m3 lowest
-Weights(AICc(m2_top, m3_top))# m3 clear top model # m_top & m_2 are same model so reporting one
+AICc(m, m2, m3)# m3 lowest
+model.sel(m, m2, m3)
+Weights(AICc(m, m2, m3))# m3 clear top model
 
-r.squaredGLMM(m3_top)
+r.squaredGLMM(m3)
 
-summary(m3_top)
+summary(m3)
 
-# sjPlot::tab_model(m3_top, file = "tabOutputs/spp_specific_LMM.doc")
+# sjPlot::tab_model(m3, file = "tabOutputs/spp_specific_LMM.doc")
 
-
-peak_temp_plot <- plot_model(m3_top, terms = "rel_temp", type = "pred")
-peak_bio1Mean_plot <- plot_model(m3_top, terms = "bio1_mean", type = "pred")
-plot_model(m3_top, terms = "voltinism", type = "pred")
-plot_model(m3_top, terms = "maxWingspan", type = "pred")
-peak_bstmp_plot <- plot_model(m3_top, terms = c("rel_temp", "maxWingspan"), type = "pred")
-peak_int_plot <- plot_model(m3_top, terms = c("rel_temp", "bio1_mean"), type = "pred")
+peak_temp_plot <- plot_model(m3, terms = "rel_temp", type = "pred")
+peak_bio1Mean_plot <- plot_model(m3, terms = "bio1_mean", type = "pred")
+plot_model(m3, terms = "voltinism", type = "pred")
+plot_model(m3, terms = "maxWingspan", type = "pred")
+peak_bstmp_plot <- plot_model(m3, terms = c("rel_temp", "maxWingspan [-0.75,0,0.75]"), type = "pred")
+peak_int_plot <- plot_model(m3, terms = c("rel_temp", "bio1_mean [-1,0,1]"), type = "pred")
 
 ## make manuscript figures
 # first for peak
@@ -213,9 +217,9 @@ b <- ggplot(peak_bio1Mean_plot_df) +
 
 mdf_plot <- mdf %>% 
   mutate(group = case_when(
-    bio1_mean < -0.5 ~ "-0.98",
-    bio1_mean >= -0.5 & bio1_mean <= 0.5 ~ "-0.01",
-    bio1_mean > 0.5 ~ "0.97"
+    bio1_mean < -0.5 ~ "-1",
+    bio1_mean >= -0.5 & bio1_mean <= 0.5 ~ "0",
+    bio1_mean > 0.5 ~ "1"
   ))
 
 library(ggnewscale)
@@ -239,9 +243,9 @@ c
 
 mdf_plot2 <- mdf %>% 
   mutate(group = case_when(
-    maxWingspan < -0.5 ~ "-0.73",
+    maxWingspan < -0.5 ~ "-0.75",
     maxWingspan >= -0.5 & maxWingspan <= 0.5 ~ "0",
-    maxWingspan > 0.5 ~ "0.73"
+    maxWingspan > 0.5 ~ "0.75"
   ))
 
 d <- ggplot(peak_bstmp_plot_df) +
@@ -264,3 +268,30 @@ d
 cp2 <- cowplot::plot_grid(c,d, labels = c("A", "B"), nrow = 2, ncol = 1)
 ggsave(plot = cp2, filename = "figOutputs/Fig3_sppSpecificIntercations.png", dpi = 450,
        width = 5.5, height = 7)
+
+
+## write tables for three competing models
+m_df <- as.data.frame(summary(m)$coefficients)
+m_ci <- as.data.frame(confint(m)[3:14,])
+m_df$Lower.CI <- as.vector(m_ci$`2.5 %`)
+m_df$Upper.CI <- as.vector(m_ci$`97.5 %`)
+m_df <- rownames_to_column(m_df)
+
+write.csv(m_df, "tabOutputs/Dev1_sppSpecificEffects.csv", row.names = F)
+
+m2_df <- as.data.frame(summary(m2)$coefficients)
+m2_ci <- as.data.frame(confint(m2)[3:14,])
+m2_df$Lower.CI <- as.vector(m2_ci$`2.5 %`)
+m2_df$Upper.CI <- as.vector(m2_ci$`97.5 %`)
+m2_df <- rownames_to_column(m2_df)
+
+
+write.csv(m2_df, "tabOutputs/Dev10_sppSpecificEffects.csv", row.names = F)
+
+m3_df <- as.data.frame(summary(m3)$coefficients)
+m3_ci <- as.data.frame(confint(m3)[3:14,])
+m3_df$Lower.CI <- as.vector(m3_ci$`2.5 %`)
+m3_df$Upper.CI <- as.vector(m3_ci$`97.5 %`)
+m3_df <- rownames_to_column(m3_df)
+
+write.csv(m3_df, "tabOutputs/relTemp_sppSpecificEffects.csv", row.names = F)
