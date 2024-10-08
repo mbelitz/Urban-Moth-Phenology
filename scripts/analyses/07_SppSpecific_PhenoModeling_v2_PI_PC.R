6a5d7be993513c65a9ca022a4ccdd769767450b9
mbelitz <mbelitz@ufl.edu>
2024-10-08 13:41:03 -0400

    adjustments for second revision

--- scripts/analyses/07_SppSpecific_PhenoModeling_v2_PI_PC.R
+++ scripts/analyses/07_SppSpecific_PhenoModeling_v2_PI_PC.R
@@ -14,7 +14,7 @@ l_nb <- list.files("data/sppSpecific_gamOutputsCSVs_negBinom/", full.names = T)
 
 gam_df_nb <- l_nb %>% 
   map_df(~read_csv(.x)) 
-         
+
 gam_df_nb <- gam_df_nb %>% 
   filter(!is.na(fit)) %>% 
   mutate(SiteSpp = paste(Site, validName, sep = "_"))
@@ -135,22 +135,18 @@ performance::check_collinearity(m3)
 summary(m3)
 confint(m3)
 
-AICc(m, m3)# m3 lowest
-model.sel(m, m3)
-Weights(AICc(m, m3))# m3 clear top model
-
 r.squaredGLMM(m3)
 
 summary(m3)
 
 # sjPlot::tab_model(m3, file = "tabOutputs/spp_specific_LMM.doc")
 # SJ PLOT MODEL PREDICTIONS FOR Impervious surface
-PI_BS_plot <- plot_model(m, terms = c("propImpervious300m", "maxWingspan [-0.75,0,0.75]"), type = "pred")
+PI_BS_plot <- plot_model(m, terms = c("propImpervious300m", "maxWingspan [-1,0,1]"), type = "pred")
 PI_TN_plot <- plot_model(m, terms = c("propImpervious300m", "bio1_mean [-1,0,1]"), type = "pred")
 PI_BS_plot_df <- PI_BS_plot$data
 PI_TN_plot_df <- PI_TN_plot$data
 
-CP_BS_plot <- plot_model(m3, terms = c("propCanopy300m", "maxWingspan [-0.75,0,0.75]"), type = "pred")
+CP_BS_plot <- plot_model(m3, terms = c("propCanopy300m", "maxWingspan [-1,0,1]"), type = "pred")
 CP_TN_plot <- plot_model(m3, terms = c("propCanopy300m", "bio1_mean [-1,0,1]"), type = "pred")
 CP_BS_plot_df <- CP_BS_plot$data
 CP_TN_plot_df <- CP_TN_plot$data
@@ -172,16 +168,39 @@ mdf_plot2 <- mdf %>%
 
 library(ggnewscale)
 
+### backtransform variables for easier interpretation 
+mean_propImpervious <- mean(mdf_raw$propImpervious300m)
+sd_propImpervious <- sd(mdf_raw$propImpervious300m)
+
+mean_propCanopy <- mean(mdf_raw$propCanopy300m)
+sd_propCanopy <- sd(mdf_raw$propCanopy300m)
+
+mean(mdf_raw$maxWingspan)
+sd(mdf_raw$maxWingspan)
+
+
+anova(re_only, m)
+anova(re_only, m3)
+
+mean(mdf_raw$bio1_mean, na.rm = T)
+mean(mdf_raw$bio1_mean, na.rm = T) + sd(mdf_raw$bio1_mean, na.rm = T)
+mean(mdf_raw$bio1_mean, na.rm = T) - sd(mdf_raw$bio1_mean, na.rm = T)
+
 a <- ggplot(PI_TN_plot_df) +
-  geom_jitter(mdf_plot, mapping = aes(x = propImpervious300m, y = peak, color = group), alpha = 0.3) + 
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
-  geom_line(aes(x = x, y = predicted, color = group)) +
+  geom_jitter(mdf_plot, mapping = aes(x = mean_propImpervious + propImpervious300m * sd_propImpervious,
+                                      y = peak), alpha = 0.3) + 
+  geom_ribbon(aes(x = mean_propImpervious + x * sd_propImpervious, ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
+  geom_line(aes(x = mean_propImpervious + x * sd_propImpervious, y = predicted, color = group)) +
   labs(y = "Peak DOY", x = "Proportion impervious surface (300 m)", 
        fill = "Temp niche", color = "Temp niche") +
   scale_fill_manual(values = c("#2A9D8F", "#D5A220", "#E76F51"),
-                    labels = c("-1 (Cold-adapted)", "0 (Average)", "1 (Warm-adapted)")) +
+                    labels = c( paste0("10.9", "\u00B0", "C", "\n", "(Cold-adapted)"), 
+                                paste0("13.9", "\u00B0", "C", "\n", "(Average)"),
+                                paste0("16.8", "\u00B0", "C", "\n", "(Warm-Adapted)"))) +
   scale_color_manual(values = c("#2A9D8F", "#D5A220", "#E76F51"),
-                     labels = c("-1 (Cold-adapted)", "0 (Average)", "1 (Warm-adapted)")) +
+                     labels = c( paste0("10.9", "\u00B0", "C", "\n", "(Cold-adapted)"), 
+                                 paste0("13.9", "\u00B0", "C", "\n", "(Average)"),
+                                 paste0("16.8", "\u00B0", "C", "\n", "(Warm-Adapted)")))  +
   theme_classic() +
   theme(legend.position = "none",
         axis.text=element_text(size=14),
@@ -189,15 +208,18 @@ a <- ggplot(PI_TN_plot_df) +
 a
 
 b <- ggplot(PI_BS_plot_df) +
-  geom_jitter(mdf_plot2, mapping = aes(x = propImpervious300m, y = peak, color = group), alpha = 0.3) + 
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
-  geom_line(aes(x = x, y = predicted, color = group)) +
+  geom_jitter(mdf_plot2, mapping = aes(x = mean_propImpervious + propImpervious300m * sd_propImpervious,
+                                       y = peak), alpha = 0.3) + 
+  geom_ribbon(aes(x = mean_propImpervious + x * sd_propImpervious,
+                  ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
+  geom_line(aes(x = mean_propImpervious + x * sd_propImpervious,
+                y = predicted, color = group)) +
   labs(y = "Peak DOY", x = "Proportion impervious surface (300 m)", 
        fill = "Body size", color = "Body size") +
   scale_fill_manual(values = c("#E56399","#28262C","#DE6E4B"), 
-                    labels = c("-0.75 (Small)", "0 (Average)", "0.75 (Large)")) +
+                    labels = c("14.7mm\n(Small)", "22.6mm\n(Average)", "30.6mm\n(Large)")) +
   scale_color_manual(values = c("#E56399","#28262C","#DE6E4B"), 
-                     labels = c("-0.75 (Small)", "0 (Average)", "0.75 (Large)")) +
+                     labels = c("14.7mm (Small)", "22.6mm\n(Average)", "30.6mm\n(Large)")) +
   theme_classic() +
   theme(legend.position = "none",
         axis.text=element_text(size=14),
@@ -207,47 +229,58 @@ b
 # Canopy cover
 
 c <- ggplot(CP_TN_plot_df) +
-  geom_jitter(mdf_plot, mapping = aes(x = propCanopy300m, y = peak, color = group), alpha = 0.3) + 
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
-  geom_line(aes(x = x, y = predicted, color = group)) +
+  geom_jitter(mdf_plot, mapping = aes(x = mean_propCanopy + propCanopy300m * sd_propCanopy,
+                                      y = peak), alpha = 0.3) + 
+  geom_ribbon(aes(x = mean_propCanopy + x * sd_propCanopy, 
+                  ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
+  geom_line(aes(x = mean_propCanopy + x * sd_propCanopy, y = predicted, color = group)) +
   labs(y = "Peak DOY", x = "Proportion canopy cover (300 m)", 
        fill = "Temp niche", color = "Temp niche") +
   scale_fill_manual(values = c("#2A9D8F", "#D5A220", "#E76F51"),
-                    labels = c("-1 (Cold-adapted)", "0 (Average)", "1 (Warm-adapted)")) +
+                    labels = c( paste0("10.9", "\u00B0", "C", "\n", "(Cold\nadapted)"), 
+                                paste0("13.9", "\u00B0", "C", "\n", "(Average)"),
+                                paste0("16.8", "\u00B0", "C", "\n", "(Warm\nAdapted)"))) +
   scale_color_manual(values = c("#2A9D8F", "#D5A220", "#E76F51"),
-                     labels = c("-1 (Cold-adapted)", "0 (Average)", "1 (Warm-adapted)")) +
+                     labels = c( paste0("10.9", "\u00B0", "C", "\n", "(Cold\nadapted)"), 
+                                 paste0("13.9", "\u00B0", "C", "\n", "(Average)"),
+                                 paste0("16.8", "\u00B0", "C", "\n", "(Warm\nAdapted)")))  +
   theme_classic() +
   theme(legend.position = "bottom",
         axis.text=element_text(size=14),
         axis.title=element_text(size=16),
-        legend.text = element_text(size = 14),
-        legend.title = element_text(size = 15))
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 11,
+                                    face = "bold"))
 
 c
 
 d <- ggplot(CP_BS_plot_df) +
-  geom_jitter(mdf_plot2, mapping = aes(x = propCanopy300m, y = peak, color = group), alpha = 0.3) + 
-  geom_ribbon(aes(x = x, ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
-  geom_line(aes(x = x, y = predicted, color = group)) +
+  geom_jitter(mdf_plot2, mapping = aes(x = mean_propCanopy + propCanopy300m * sd_propCanopy,
+                                       y = peak), alpha = 0.3) + 
+  geom_ribbon(aes(x = mean_propCanopy + x * sd_propCanopy,
+                  ymin = conf.low, ymax = conf.high, fill = group), alpha = 0.25) +
+  geom_line(aes(x = mean_propCanopy + x * sd_propCanopy,
+                y = predicted, color = group)) +
   labs(y = "Peak DOY", x = "Proportion canopy cover (300 m)", 
        fill = "Body size", color = "Body size") +
   scale_fill_manual(values = c("#E56399","#28262C","#DE6E4B"), 
-                    labels = c("-0.75 (Small)", "0 (Average)", "0.75 (Large)")) +
+                    labels = c("14.7mm\n(Small)", "22.6mm\n(Average)", "30.6mm\n(Large)")) +
   scale_color_manual(values = c("#E56399","#28262C","#DE6E4B"), 
-                     labels = c("-0.75 (Small)", "0 (Average)", "0.75 (Large)")) +
+                     labels = c("14.7mm\n(Small)", "22.6mm\n(Average)", "30.6mm\n(Large)")) +
   theme_classic() +
   theme(legend.position = "bottom",
         axis.text=element_text(size=14),
         axis.title=element_text(size=16),
-        legend.text = element_text(size = 14),
-        legend.title = element_text(size = 15))
+        legend.text = element_text(size = 10),
+        legend.title = element_text(size = 11, 
+                                    face = "bold"))
 d
 
 cp2 <- cowplot::plot_grid(a,b,c,d, labels = c("A", "B", "C", "D"), 
-                         nrow = 2, ncol = 2)
-ggsave(plot = cp2, filename = "figOutputs/resubmission/Fig2_sppSpecificIntercations_v2_negBinom.png", 
+                          nrow = 2, ncol = 2)
+ggsave(plot = cp2, filename = "figOutputs/resubmission/resubmissionSept/Fig2_sppSpecificIntercations_v2_negBinom.png", 
        dpi = 450,
-       width = 13.5, height = 9)
+       width = 8.3, height = 8.3)
 
 
 ## write tables for three competing models
